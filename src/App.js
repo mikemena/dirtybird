22aa5bc21835bb849b512a4f07e0fadeff357458
cooper <tokyolabs@protonmail.com>
2022-05-21 13:59:54 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 const Thing = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Dirty Bird')
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Dirty Bird"),
   ]);
 };
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Thing)
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Thing),
   ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
