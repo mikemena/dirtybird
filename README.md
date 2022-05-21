7ca911ca882211c9d97c7af0e86046a844a88407
cooper <tokyolabs@protonmail.com>
2022-05-21 13:15:45 -0400

    commit

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Course repo:
 
 https://btholt.github.io/complete-intro-to-react-v7/
+
+Course link:
+
+https://frontendmasters.com/courses/complete-react-v7/app-component/
---
+++ src/App.js
@@ -0,0 +1,13 @@
+const Thing = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Dirty Bird')
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Thing)
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
