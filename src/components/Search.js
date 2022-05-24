8a4b30fda67efbc5d068eb816a3c29bc0e109916
cooper <tokyolabs@protonmail.com>
2022-05-24 17:02:08 -0400

    commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,59 +1,25 @@
 import { useState, useEffect } from "react";
 
-const ITEMS = ["Asic", "Immersion"];
-
 const Search = () => {
-  const [location, setLocation] = useState("");
-  const [item, setItem] = useState("");
-  console.log(location);
-
-  // Fetch Items
-  const fetchItems = async () => {
-    const res = await fetch(`http://localhost:3001/items`);
-    const items = await res.json();
-    return items;
-  };
+  const [items, setItems] = useState([]);
+  // console.log(location);
 
   useEffect(() => {
-    requestItems;
-  });
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  async function requestPets() {
+    const res = await fetch(`http://localhost:3001/items`);
+    const items = await res.json();
+    setItems(items);
+    console.log(items);
+  }
 
   return (
-    <div className="search-params">
-      <form>
-        {/* <label htmlFor="location">
-          Location {location}
-          <input
-            id="location"
-            value={location}
-            placeholder="Location"
-            onChange={(e) => setLocation(e.target.value)}
-          />
-        </label> */}
-        <label htmlFor="item">
-          item
-          <select
-            id="item"
-            value={item}
-            onChange={(e) => {
-              setItem(e.target.value);
-            }}
-            onBlur={(e) => {
-              setItem(e.target.value);
-            }}
-          >
-            <option />
-            {ITEMS.map((item) => {
-              return (
-                <option key={item} value={item}>
-                  {item}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-        <button>Submit</button>
-      </form>
+    <div>
+      {items.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
     </div>
   );
 };
