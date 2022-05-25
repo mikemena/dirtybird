e77fcf858ebd48e5ce448bddee2c8593b2805368
cooper <tokyolabs@protonmail.com>
2022-05-25 14:45:12 -0400

    commit

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,13 +1,29 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Details = () => {
+  const [item, setItem] = useState([]);
+  // console.log(location);
+
+  useEffect(() => {
+    requestItem();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  async function requestItem() {
+    const res = await fetch(`http://localhost:3001/items/${id}`);
+    const item = await res.json();
+    setItem(item);
+    console.log(item);
+  }
+
   const { id } = useParams();
   return (
     <div>
       <h2>{id}</h2>
       <Link to="/">
         <p>back</p>
+        <p>{`${item.name}-${item.material}-${item.details}-${item.price}`}</p>
       </Link>
     </div>
   );
